75b5beee19b1ca52b4a0081aeb4364ce76453ec1
Jorge L. Rivas <jorge.lopez@karalundi.com.mx>
2019-09-09 17:08:05 -0500

    reglas eslint

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,4 +1,41 @@
 module.exports = {
-  root: true,
-  extends: '@react-native-community',
-};
+    "parser": "babel-eslint",
+    "parserOptions": { 
+        "ecmaVersion": 6
+    },
+    "env": {
+        "browser": true,
+        "amd": true,
+        "jest": true,
+        "es6": true,
+        "node": true
+    },
+    "plugins": [
+        "react"
+    ],
+    "extends": [
+        "eslint:recommended",
+        "plugin:react/recommended"
+    ],
+    "rules": {
+		"comma-dangle": ["error", {
+        "arrays": "always",
+        "objects": "always",
+		}],
+        "no-trailing-spaces":"error",
+        "jsx-quotes": ["error", "prefer-single"],
+        "quotes": ["error", "single"],
+        "no-multi-spaces": "error",
+        "indent": ["error", 4,  { "SwitchCase": 1 }],
+        "curly": "error",
+        "no-var": "error",
+        "semi": ["error", "always"],
+        "comma-spacing": ["error", { "before": false, "after": true }],
+        "react/prop-types": ["error", { "ignore": ["navigation"] }],
+        "keyword-spacing": ["error", { "before": true, "after": true }],
+        "brace-style": ["error", "stroustrup"]
+    },
+    "globals": {
+        "__DEV__": true
+    }
+}
