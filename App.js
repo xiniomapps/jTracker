ce373efc4bc350740956841d11fe2e82eebd0431
Jorge L. Rivas <jorge.lopez@karalundi.com.mx>
2019-09-09 18:43:10 -0500

    Nevegacion

--- App.js
+++ App.js
@@ -6,109 +6,60 @@
  * @flow
  */
 
-import React, {Fragment} from 'react';
-import {
-  SafeAreaView,
-  StyleSheet,
-  ScrollView,
-  View,
-  Text,
-  StatusBar,
-} from 'react-native';
+import React, {Component} from 'react';
+import {createAppContainer} from 'react-navigation';
+import {createDrawerNavigator} from 'react-navigation-drawer';
+import { createStackNavigator} from 'react-navigation-stack';
+import NavigationService from './src/utils/NavigatorService';
+import MainScreen from './src/screens/MainScreen';
+import MetricasScreen from './src/screens/MetricasScreen';
+import { Icon } from 'react-native-elements';
 
-import {
-  Header,
-  LearnMoreLinks,
-  Colors,
-  DebugInstructions,
-  ReloadInstructions,
-} from 'react-native/Libraries/NewAppScreen';
-
-const App = () => {
-  return (
-    <Fragment>
-      <StatusBar barStyle="dark-content" />
-      <SafeAreaView>
-        <ScrollView
-          contentInsetAdjustmentBehavior="automatic"
-          style={styles.scrollView}>
-          <Header />
-          {global.HermesInternal == null ? null : (
-            <View style={styles.engine}>
-              <Text style={styles.footer}>Engine: Hermes</Text>
-            </View>
-          )}
-          <View style={styles.body}>
-            <View style={styles.sectionContainer}>
-              <Text style={styles.sectionTitle}>Step One</Text>
-              <Text style={styles.sectionDescription}>
-                Edit <Text style={styles.highlight}>App.js</Text> to change this
-                screen and then come back to see your edits.
-              </Text>
-            </View>
-            <View style={styles.sectionContainer}>
-              <Text style={styles.sectionTitle}>See Your Changes</Text>
-              <Text style={styles.sectionDescription}>
-                <ReloadInstructions />
-              </Text>
-            </View>
-            <View style={styles.sectionContainer}>
-              <Text style={styles.sectionTitle}>Debug</Text>
-              <Text style={styles.sectionDescription}>
-                <DebugInstructions />
-              </Text>
-            </View>
-            <View style={styles.sectionContainer}>
-              <Text style={styles.sectionTitle}>Learn More</Text>
-              <Text style={styles.sectionDescription}>
-                Read the docs to discover what to do next:
-              </Text>
-            </View>
-            <LearnMoreLinks />
-          </View>
-        </ScrollView>
-      </SafeAreaView>
-    </Fragment>
-  );
+const navOptions = ({navigation, }) => {
+    return {
+        title: 'jTracker',
+        gesturesEnabled: true,
+        headerStyle: {
+            backgroundColor: '#069',
+        },
+        headerTintColor: '#f0f0f0',
+        headerLeft: (
+            <Icon
+                name='menu'
+                type='material'
+                color='#f0f0f0'
+                onPress={() => navigation.toggleDrawer()}
+            />
+        ),
+    };
 };
 
-const styles = StyleSheet.create({
-  scrollView: {
-    backgroundColor: Colors.lighter,
-  },
-  engine: {
-    position: 'absolute',
-    right: 0,
-  },
-  body: {
-    backgroundColor: Colors.white,
-  },
-  sectionContainer: {
-    marginTop: 32,
-    paddingHorizontal: 24,
-  },
-  sectionTitle: {
-    fontSize: 24,
-    fontWeight: '600',
-    color: Colors.black,
-  },
-  sectionDescription: {
-    marginTop: 8,
-    fontSize: 18,
-    fontWeight: '400',
-    color: Colors.dark,
-  },
-  highlight: {
-    fontWeight: '700',
-  },
-  footer: {
-    color: Colors.dark,
-    fontSize: 12,
-    fontWeight: '600',
-    padding: 4,
-    paddingRight: 12,
-    textAlign: 'right',
-  },
-});
+const drawerScreens = createDrawerNavigator(
+    {
+        Principal: MainScreen,
+        Metricas: MetricasScreen,
+    },
+    {
+        initialRouteName: 'Principal',
+    }
+);
 
-export default App;
+const AppContainer = createAppContainer(createStackNavigator(
+    {
+        drawer: drawerScreens,
+    },
+    {
+        initialRouteName: 'drawer',
+        defaultNavigationOptions: navOptions,
+    }
+));
+
+export default class App extends Component {
+    render() {
+        return (
+            <AppContainer ref={(navigatorRef) => {
+                NavigationService.setTopLevelNavigator(navigatorRef);
+            }} />
+        );
+    }
+}
---
+++ src/screens/MainScreen.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import { Text, SafeAreaView } from 'react-native';
+
+class MainScreen extends Component {
+    render() {
+        return (
+            <SafeAreaView>
+                <Text> Main Screen </Text>
+            </SafeAreaView>
+        );
+    }
+}
+
+export default MainScreen;
