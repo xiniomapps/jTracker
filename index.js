e8f5277569fe735b757dee213e2d407d5edd3dc7
Jorge L. Rivas <jorge.lopez@karalundi.com.mx>
2019-09-30 12:21:45 -0500

    implement redux-persist

--- index.js
+++ index.js
@@ -7,11 +7,15 @@ import {AppRegistry} from 'react-native';
 import App from './App';
 import {name as appName} from './app.json';
 import { Provider as StoreProvider } from 'react-redux';
-import globalReduxStore from './src/redux/Store';
+import { PersistGate } from 'redux-persist/integration/react';
+
+import { globalReduxStore, persistor } from './src/redux/Store';
 
 const ReduxApp = () => (
     <StoreProvider store={globalReduxStore}>
-        <App />
+        <PersistGate persistor={persistor}>
+            <App />
+        </PersistGate>
     </StoreProvider>
 );
 
--- src/redux/Store.js
+++ src/redux/Store.js
@@ -1,21 +1,32 @@
 import { createStore, combineReducers, applyMiddleware } from 'redux';
 import createSagaMiddleware from 'redux-saga';
 import { composeWithDevTools } from 'redux-devtools-extension';
+import { persistReducer, persistStore } from 'redux-persist';
+import { AsyncStorage } from 'react-native';
+
 import rootSaga from '../sagas/rootSaga';
 import metricasReducer from '../redux/metricasReducer';
 import readingsReducer from '../redux/readingsReducer';
 
-const rootReducer = combineReducers(
+
+const persistedReducer = persistReducer(
     {
+        // Configuration
+        key: 'root',
+        storage: AsyncStorage,
+    },
+    combineReducers({
+        // Reductores
         metricas: metricasReducer,
         readings: readingsReducer,
-    }
+    })
 );
 
 const sagaMiddleware = createSagaMiddleware();
-const globalReduxStore = createStore(rootReducer, composeWithDevTools(
+let globalReduxStore = createStore(persistedReducer, composeWithDevTools(
     applyMiddleware(sagaMiddleware)
 ));
-
+let persistor = persistStore(globalReduxStore);
 sagaMiddleware.run(rootSaga);
-export default globalReduxStore;
+
+export {globalReduxStore, persistor};
