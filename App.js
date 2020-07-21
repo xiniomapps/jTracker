e68e381cfc6761561caf707f6326d3b6bc2bdc25
Jorge L. Rivas <jorge.lopez@karalundi.com.mx>
2020-07-21 16:40:11 -0500

    add basic screen tabs

--- App.js
+++ App.js
@@ -1,81 +1,41 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- * @flow
- */
 
-import React, {Component} from 'react';
-import {createAppContainer} from 'react-navigation';
-import {createDrawerNavigator} from 'react-navigation-drawer';
-import { createStackNavigator} from 'react-navigation-stack';
-import NavigationService from './src/utils/NavigatorService';
-import MainScreen from './src/screens/MainScreen';
+import React from 'react';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import { Icon } from 'react-native-elements';
+
 import MetricsScreen from './src/screens/MetricsScreen';
 import addMetricScreen from './src/screens/addMetricScreen';
 import MetricDetailsScreen from './src/screens/MetricDetailsScreen';
-import ReadingDetailsScreen from './src/screens/ReadingDetailsScreen';
-import { Icon } from 'react-native-elements';
-
-const navOptions = ({navigation, }) => {
-    return {
-        title: 'jTracker',
-        gesturesEnabled: true,
-        headerStyle: {
-            backgroundColor: '#069',
-        },
-        headerTintColor: '#f0f0f0',
-        headerLeft: (
-            <Icon
-                name='menu'
-                type='material'
-                color='#f0f0f0'
-                onPress={() => navigation.toggleDrawer()}
-            />
-        ),
-    };
-};
-
-const principalStack = createStackNavigator(
-    {
-        MainScreen,
-    },
-    {
-        defaultNavigationOptions: navOptions,
-    }
-);
-
-const metricsStack = createStackNavigator(
-    {
-        MetricsScreen,
-        addMetricScreen,
-        MetricDetailsScreen,
-        ReadingDetailsScreen,
-    },
-    {
-        defaultNavigationOptions: navOptions,
-    }
-);
-
-const drawer = createDrawerNavigator(
-    {
-        Principal: principalStack,
-        Metrics: metricsStack,
-    },
-    {
-        initialRouteName: 'Principal',
-    }
-);
-const AppContainer = createAppContainer(drawer);
 
 
-export default class App extends Component {
-    render() {
-        return (
-            <AppContainer ref={(navigatorRef) => {
-                NavigationService.setTopLevelNavigator(navigatorRef);
-            }} />
-        );
-    }
+const MetricsStack = createStackNavigator();
+function MetricsStackScreen() {
+    return (
+        <MetricsStack.Navigator>
+            <MetricsStack.Screen name='MetricsScreen' component={MetricsScreen} options={{ title: 'My Metrics', }} />
+            <MetricsStack.Screen name='addMetricScreen' component={addMetricScreen} options={{ title: 'Add New Metric', }}/>
+            <MetricsStack.Screen name='MetricDetailsScreen' component={MetricDetailsScreen} options={{ title: 'Metric Details', }}/>
+        </MetricsStack.Navigator>
+    );
+}
+
+const Tab = createBottomTabNavigator();
+
+export default function App() {
+    return (
+        <NavigationContainer>
+            <Tab.Navigator>
+                <Tab.Screen name='My Metrics' component={MetricsStackScreen}
+                    options={{
+                        // eslint-disable-next-line react/display-name
+                        tabBarIcon: () => (
+                            <Icon type='material-community' name='chart-line'/>
+                        ),
+                    }}
+                />
+            </Tab.Navigator>
+        </NavigationContainer>
+    );
 }
